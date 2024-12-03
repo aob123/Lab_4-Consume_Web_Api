dcf3a93bc4f26c53b844d6a9591fb47cc76e65dd
aob123 <alexobeswick@gmail.com>
2024-12-03 10:06:30 +0100

    Updated user agent and CW

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -4,18 +4,16 @@ HttpClient client = new();
 
 try
 {
+    client.DefaultRequestHeaders.Add("User-Agent", "aob123");
     var url = new Uri("https://api.github.com/orgs/dotnet/repos");
     var response = await client.GetStringAsync(url);
-    client.DefaultRequestHeaders.Add("User-Agent", "aob123");
     int counter = 0;
     var repos = JsonSerializer.Deserialize<List<Repo>>(response);
 
     foreach (var repo in repos)
     {
         counter++;
-        Console.WriteLine($"Number: {counter}");
-        Console.WriteLine($"Name: {repo.Name} \n Description: {repo.Description} \n url: {repo.html_url} \n homepage: {repo.Homepage} \n watchers: {repo.Watchers} \n pushed: {repo.Pushed_at} \n\n");
-
+        Console.WriteLine($"#{counter} \n {repo}");
     }
 
 }
